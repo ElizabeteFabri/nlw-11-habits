57badd0fad365e6a288e9406ea894337965cd2a2
Elizabete Fabri <elizabete958@gmail.com>
2023-01-21 21:00:50 -0300

    Aula 03 - ReactJS

--- frontend/src/components/HabitDay.tsx
+++ frontend/src/components/HabitDay.tsx
@@ -1,9 +1,40 @@
-// interface HabitDayProps {
-//   completed: number
-// }
+import * as Popover from "@radix-ui/react-popover";
+import clsx from "clsx";
+import { ProgressBar } from "./ProgressBar";
 
-export function HabitDay() {
+
+interface HabitDayProps {
+  completed: number
+  amount: number
+}
+
+export function HabitDay({ completed, amount }: HabitDayProps) {
+
+  const completedPercent =  Math.round((completed / amount) * 100);
+
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    <Popover.Root>
+      <Popover.Trigger
+        className={clsx ("w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg", {
+          'bg-zinc-900 border-zinc-800': completedPercent === 0,
+          'bg-violet-900 border-violet-500': completedPercent > 0 && completedPercent < 20,
+          'bg-violet-800 border-violet-500': completedPercent >= 20 && completedPercent < 40,
+          'bg-violet-700 border-violet-500': completedPercent >= 40 && completedPercent < 60,
+          'bg-violet-600 border-violet-500': completedPercent >= 60 && completedPercent < 80,
+          'bg-violet-500 border-violet-400': completedPercent >= 80,
+        })}
+      />
+
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+          <span className="font-semibold text-zinc-400">terça-feira</span>
+          <span className="mt-1 font-extrabold leading-tight text-3xl">17/01</span>
+
+          <ProgressBar progress={completedPercent} />
+
+          <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 }
